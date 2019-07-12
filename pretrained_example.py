a578d76de2fdf2053b053fd964c5f2afe45197cc
Evan Thomas <thomas.e@wehi.edu.au>
2019-07-12 12:39:47 +1000

    determnistic walk in latent space for image generation

--- pretrained_example.py
+++ pretrained_example.py
@@ -16,36 +16,39 @@ import dnnlib.tflib as tflib
 import config
 
 def main():
-    # Initialize TensorFlow.
-    tflib.init_tf()
+  # Initialize TensorFlow.
+  tflib.init_tf()
 
-    # Load pre-trained network.
-    # url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    # with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
-    url = '/home/evan/neurosim/stylegan/results/00001-sgan-evan-1024-2gpu/network-snapshot-025000.pkl'
-    with open(url, "rb") as f:
-        _G, _D, Gs = pickle.load(f)
-        # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
-        # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
-        # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
+  # Load pre-trained network.
+  # url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
+  # with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+  url = 'results/00001-sgan-evan-1024-2gpu/network-snapshot-025000.pkl'
+  with open(url, "rb") as f:
+    _G, _D, Gs = pickle.load(f)
+    # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
+    # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
+    # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
 
-    # Print network details.
-    Gs.print_layers()
+  # Print network details.
+  Gs.print_layers()
 
-    rnd = np.random.RandomState()
+  rnd = np.random.RandomState()
+  N = Gs.input_shape[1]
+  latents = rnd.randn(1, N)
+  delta = 1e-2
+  direction = rnd.randint(0, N)
 
-    for i in range(1000):
-        # Pick latent vector.
-        latents = rnd.randn(1, Gs.input_shape[1])
+  for i in range(1000):
+    latents[0, direction] += delta
 
-        # Generate image.
-        fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-        images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+    # Generate image.
+    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
-        # Save image.
-        os.makedirs(config.result_dir, exist_ok=True)
-        png_filename = os.path.join(config.result_dir, 'example%04d.png' % i)
-        PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+    # Save image.
+    os.makedirs(config.result_dir, exist_ok=True)
+    png_filename = os.path.join(config.result_dir, 'example%04d.png' % i)
+    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
 
 if __name__ == "__main__":
-    main()
+  main()
